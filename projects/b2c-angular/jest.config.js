3a2b85072ebfecb2bbfacd91ca5ad66bf9490283
Hendrik Wouters <hendrik.wouters@live.nl>
2024-05-01 18:42:15 +0000

    Update packages

--- projects/b2c-angular/jest.config.js
+++ projects/b2c-angular/jest.config.js
@@ -2,17 +2,18 @@ module.exports = {
   testEnvironment: 'jsdom',
   displayName: 'b2c-angular',
   setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.html$',
-    },
-  },
+  globals: {},
   testMatch: ['<rootDir>/**/*.spec.ts'],
   moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
   resolver: 'jest-preset-angular/build/resolvers/ng-jest-resolver.js',
   transformIgnorePatterns: ['<rootDir>../../node_modules/(?!.*\\.mjs$)'],
   transform: {
-    '^.+\\.(ts|mjs|html|svg)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|html|svg)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.html$',
+      },
+    ],
   },
 };
